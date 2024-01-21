7c01b6b3238a57aa0d4a89d86c3c83390f234ac7
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-01-21 04:27:52 +0300

    GoturAngular

---
+++ node_modules/ngx-quill/config/public_api.d.ts
@@ -0,0 +1,3 @@
+export * from './quill-defaults';
+export * from './quill-config.module';
+export * from './quill-editor.interfaces';
---
+++ node_modules/ngx-quill/esm2020/config/public_api.mjs
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-quill/config
+ */
+export * from './quill-defaults';
+export * from './quill-config.module';
+export * from './quill-editor.interfaces';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3Byb2plY3RzL25neC1xdWlsbC9jb25maWcvc3JjL3B1YmxpY19hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFFSCxjQUFjLGtCQUFrQixDQUFBO0FBQ2hDLGNBQWMsdUJBQXVCLENBQUE7QUFDckMsY0FBYywyQkFBMkIsQ0FBQSIsInNvdXJjZXNDb250ZW50IjpbIi8qXG4gKiBQdWJsaWMgQVBJIFN1cmZhY2Ugb2Ygbmd4LXF1aWxsL2NvbmZpZ1xuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcXVpbGwtZGVmYXVsdHMnXG5leHBvcnQgKiBmcm9tICcuL3F1aWxsLWNvbmZpZy5tb2R1bGUnXG5leHBvcnQgKiBmcm9tICcuL3F1aWxsLWVkaXRvci5pbnRlcmZhY2VzJ1xuIl19
